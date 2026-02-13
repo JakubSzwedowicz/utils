8a1692ca42f7750efc5149df8db04ceccc99cbee
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2026-02-13 02:55:25 +0100

    Fix workflows.

--- src/Logging/LoggerMacros.h
+++ src/Logging/LoggerMacros.h
@@ -8,9 +8,7 @@ namespace {
 using _Logger = Utils::Logging::Logger;
 }
 
-#define LOG(LogLevelValue, ...)                              \
-    m_logger.log<Utils::Logging::LogLevel::LogLevelValue>(   \
-                 fmt::format(__VA_ARGS__))
+#define LOG(LogLevelValue, ...) m_logger.log<Utils::Logging::LogLevel::LogLevelValue>(fmt::format(__VA_ARGS__))
 
 #define LOG_D(...) LOG(DEBUG, __VA_ARGS__)
 #define LOG_I(...) LOG(INFO, __VA_ARGS__)
